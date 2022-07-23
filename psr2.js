18433778b87cd6988be9fdcc6977506a4a085b7d
David <david-williams-1996@outlook.com>
2022-07-22 19:56:20 -0700

    Pass button.id to singleRound function

--- psr2.js
+++ psr2.js
@@ -1,9 +1,34 @@
+function computerPlay() {
+    let options = ["paper", "scissors", "rock"];
+    return options[Math.floor(Math.random()*options.length)];
+}
+
+function singleRound(userInput) {
+    let userChoice = userInput;
+    let computerChoice = computerPlay();
+
+    if (userChoice == "paper" && computerChoice == "rock" || 
+    userChoice == "rock" && computerChoice == "scissors" ||
+    userChoice == "scissors" && computerChoice == "paper") {
+        console.log(`Congratulations ${userChoice} beat ${computerChoice}!`);
+        return "Win"
+    } else if (computerChoice == userChoice) {
+        console.log("Tie! - No winner for this round");
+        return "Tie";
+    } else {
+        console.log(`Bad luck ${computerChoice} beat ${userChoice}`);
+        return "Lose";
+    }
+
+}
 
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        alert(button.id);
-     });
+        //console.log(button.id)
+        singleRound(button.id.toLowerCase());
+    });
 });
+
 
