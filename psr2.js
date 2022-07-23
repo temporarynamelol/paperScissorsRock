683ef7d7db91dc1f0b737482366818e815e44281
David <david-williams-1996@outlook.com>
2022-07-22 20:15:26 -0700

    Add comments to whole js file for readability

--- psr2.js
+++ psr2.js
@@ -1,20 +1,27 @@
+//returns random computer choice
 function computerPlay() {
     let options = ["paper", "scissors", "rock"];
+    //selects random integer index of options array  
     return options[Math.floor(Math.random()*options.length)];
 }
 
 function singleRound(userInput) {
+    //set user choice to value that is passed in when function is called 
     let userChoice = userInput;
+    //sets computer choice to result of computerPlay function
     let computerChoice = computerPlay();
 
+    //determines all scenarios where user would win when userChoice is compared to computerChoice
     if (userChoice == "paper" && computerChoice == "rock" || 
     userChoice == "rock" && computerChoice == "scissors" ||
     userChoice == "scissors" && computerChoice == "paper") {
         console.log(`Congratulations ${userChoice} beat ${computerChoice}!`);
-        return "Win"
+        return "Win"    
+    //determine tie
     } else if (computerChoice == userChoice) {
         console.log("Tie! - No winner for this round");
         return "Tie";
+    //determines when user has not won or tied 
     } else {
         console.log(`Bad luck ${computerChoice} beat ${userChoice}`);
         return "Lose";
@@ -22,13 +29,15 @@ function singleRound(userInput) {
 
 }
 
+//queries all buttons on page
 const buttons = document.querySelectorAll('button');
-
+//each time button is pushed, pass the value of the button to the singleRound function
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        //console.log(button.id)
         singleRound(button.id.toLowerCase());
     });
 });
+
+
 
 
